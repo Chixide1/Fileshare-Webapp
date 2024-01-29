723da7a3fc95f08e469f3ab8b986c4638b4de620
Chik <chixide12@gmail.com>
2024-01-29 20:02:46 +0000

    added container creation

--- Iac/main.tf
+++ Iac/main.tf
@@ -18,3 +18,17 @@ resource "azurerm_resource_group" "rg" {
   name     = "fileshare-webapp"
   location = "uksouth"
 }
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "chikfilesharewebappsa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "upload"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
