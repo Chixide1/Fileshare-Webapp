7d9bf58169f470f583c61526ee8edeb78bf4d30c
Chik <chixide12@gmail.com>
2024-02-02 22:11:22 +0000

    moved django folder up a directory

--- backend/Iac/main.tf
+++ backend/Iac/main.tf
@@ -16,13 +16,13 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "fileshare-webapp"
-  location = "uksouth"
+  location = var.default_location
 }
 
 resource "azurerm_storage_account" "sa" {
   name                     = "chikfilesharewebappsa"
   resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  location                 = var.default_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -32,3 +32,41 @@ resource "azurerm_storage_container" "container" {
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
+
+resource "azurerm_key_vault" "kv" {
+  name = "chik-fileshare-key-vault"
+  location = var.default_location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name = "standard"
+  tenant_id = "380c0008-0574-46d2-8938-093fa0b5de50"
+  soft_delete_retention_days = 7
+}
+
+resource "azuread_application" "fileshareapp-name" {
+  display_name = "Fileshare-Webapp"
+}
+
+resource "azuread_service_principal" "fileshareapp-sp" {
+  client_id = azuread_application.fileshareapp-name.client_id
+}
+
+resource "azuread_service_principal_password" "client_secret" {
+  service_principal_id = azuread_service_principal.fileshareapp-sp.object_id
+}
+
+resource "azurerm_service_plan" "app-sp" {
+  name                = "Fileshare-webapp-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.default_location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+# resource "azurerm_linux_web_app" "linux-wa" {
+#   name                = "fileshare-webapp"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = var.default_location
+#   service_plan_id     = azurerm_service_plan.app-sp.id
+
+#   site_config {}
+# }
---
+++ backend/Iac/variables.tf
@@ -0,0 +1,5 @@
+variable "default_location" {
+  type = string
+  default = "uksouth"
+  description = "The location I will use for all resources"
+}
