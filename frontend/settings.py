bf82208137a26702ba3986905df2a8688b917f15
Chik <chixide12@gmail.com>
2024-02-11 19:42:00 +0000

    removed backstroke

--- frontend/settings.py
+++ frontend/settings.py
@@ -121,14 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'website/static')
     ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
