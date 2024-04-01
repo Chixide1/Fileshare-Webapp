d98882a3f5f37077ecd7280a046571c73c4afefa
Chik <chixide12@gmail.com>
2024-04-01 22:38:18 +0100

    added local .env file

--- backend/variables.tf
+++ backend/variables.tf
@@ -18,13 +18,13 @@ resource "random_password" "db_pass" {
 
 resource "local_file" "env_file" {
   content = <<EOF
-    AZURE_CLIENT_ID = ${azuread_service_principal.fileshareapp-sp.client_id}
-    AZURE_CLIENT_SECRET = ${azuread_service_principal_password.client_secret.value}
-    AZURE_TENANT_ID = ${data.azurerm_client_config.current.tenant_id}
-    KV_URI = ${azurerm_key_vault.kv.vault_uri}
-    DJANGO_SECRET_KEY = ${random_password.password.result}
-    ACCOUNT_URL = ${azurerm_storage_account.sa.primary_blob_endpoint}
-    DB_PASS = ${random_password.db_pass.result}
+AZURE_CLIENT_ID = "${azuread_service_principal.fileshareapp-sp.client_id}"
+AZURE_CLIENT_SECRET = "${azuread_service_principal_password.client_secret.value}"
+AZURE_TENANT_ID = "${data.azurerm_client_config.current.tenant_id}"
+KV_URI = "${azurerm_key_vault.kv.vault_uri}"
+DJANGO_SECRET_KEY = "${random_password.password.result}"
+ACCOUNT_URL = "${azurerm_storage_account.sa.primary_blob_endpoint}"
+DB_PASS = "${random_password.db_pass.result}"
   EOF
 
   filename = "../.env"
