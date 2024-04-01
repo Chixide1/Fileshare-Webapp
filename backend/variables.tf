32f11fcd5e6a20b172ee940de7c7431bc93247ca
Chik <chixide12@gmail.com>
2024-04-01 22:17:33 +0100

    added local env file with required data

--- backend/variables.tf
+++ backend/variables.tf
@@ -15,3 +15,17 @@ resource "random_password" "db_pass" {
   special = true
   override_special = "!#$%&*()_=+^<>:?"
 }
+
+resource "local_file" "env_file" {
+  content = <<EOF
+    AZURE_CLIENT_ID = ${azuread_service_principal.fileshareapp-sp.client_id}
+    AZURE_CLIENT_SECRET = ${azuread_service_principal_password.client_secret.value}
+    AZURE_TENANT_ID = ${data.azurerm_client_config.current.tenant_id}
+    KV_URI = ${azurerm_key_vault.kv.vault_uri}
+    DJANGO_SECRET_KEY = ${random_password.password.result}
+    ACCOUNT_URL = ${azurerm_storage_account.sa.primary_blob_endpoint}
+    DB_PASS = ${random_password.db_pass.result}
+  EOF
+
+  filename = "../.env"
+}
