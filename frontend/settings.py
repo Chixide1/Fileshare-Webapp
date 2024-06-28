06aaed13bdb9941b65a7d34cb320f71a5612d098
Chik <chixide12@gmail.com>
2024-06-28 20:50:40 +0100

    changed static url to assets

--- frontend/settings.py
+++ frontend/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'website/static')
+        os.path.join(BASE_DIR, 'website/assets')
     ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
